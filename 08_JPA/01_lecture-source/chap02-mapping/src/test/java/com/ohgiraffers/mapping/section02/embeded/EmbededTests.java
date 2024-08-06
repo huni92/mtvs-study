edf0c5a34d07f4ee1956a649f0a2c075230869f2
Hoon <gns2862@naver.com>
2024-08-06 13:27:42 +0900

    jpa 수업6

--- 08_JPA/01_lecture-source/chap02-mapping/src/test/java/com/ohgiraffers/mapping/section02/embeded/EmbededTests.java
+++ 08_JPA/01_lecture-source/chap02-mapping/src/test/java/com/ohgiraffers/mapping/section02/embeded/EmbededTests.java
@@ -2,6 +2,7 @@ package com.ohgiraffers.mapping.section02.embeded;
 
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -47,5 +48,63 @@ public class EmbededTests {
         Assertions.assertDoesNotThrow(
                 () -> bookRegistService.registBook(bookInfo)
         );
+    }
+
+    private static Stream<Arguments> negativePrice() {
+        return Stream.of(
+                Arguments.of(
+                        "자바의정석",
+                        "남궁성",
+                        "도우출판",
+                        LocalDate.now(),
+                        -30000,
+                        0.1
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativePrice")
+    void testNegativePrice(String bookTitle, String author, String publisher, LocalDate publishedDate,
+                           int regularPrice, double discountRate) {
+
+        BookRegistRequestDTO bookInfo =
+                new BookRegistRequestDTO(bookTitle, author, publisher, publishedDate, regularPrice, discountRate);
+
+        Exception exception = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> bookRegistService.registBook(bookInfo)
+        );
+
+        Assertions.assertEquals("가격은 음수일 수 없습니다.", exception.getMessage());
+    }
+
+    private static Stream<Arguments> negativeDiscountPrice() {
+        return Stream.of(
+                Arguments.of(
+                        "자바의정석",
+                        "남궁성",
+                        "도우출판",
+                        LocalDate.now(),
+                        30000,
+                        -0.1
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativeDiscountPrice")
+    void testNegativeDiscountPrice(String bookTitle, String author, String publisher, LocalDate publishedDate,
+                                   int regularPrice, double discountRate) {
+
+        BookRegistRequestDTO bookInfo =
+                new BookRegistRequestDTO(bookTitle, author, publisher, publishedDate, regularPrice, discountRate);
+
+        Exception exception = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> bookRegistService.registBook(bookInfo)
+        );
+
+        Assertions.assertEquals("할인율은 음수일 수 없습니다.", exception.getMessage());
     }
 }
