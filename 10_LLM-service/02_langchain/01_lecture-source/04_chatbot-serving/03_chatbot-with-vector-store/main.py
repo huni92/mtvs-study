c2b575ca5cf45fd5d3d8a73a16e0bf53c6dce195
Hoon <gns2862@naver.com>
2024-09-22 12:19:46 +0900

    python 수정

--- 10_LLM-service/02_langchain/01_lecture-source/04_chatbot-serving/03_chatbot-with-vector-store/main.py
+++ 10_LLM-service/02_langchain/01_lecture-source/04_chatbot-serving/03_chatbot-with-vector-store/main.py
@@ -86,3 +86,4 @@ async def conversation(conversation_session_id: Annotated[str, Form()], conversa
     vectorstore.save_local("./db/" + conversation_session_id)
 
     return conversation_response
+
