c0f72217528d8b49fc46fbfbf86d586d592349a5
Hoon <gns2862@naver.com>
2024-12-14 16:43:19 +0900

    repository 수정

--- 08_JPA/01_lecture-source/chap04-jpql/src/main/java/com/ohgiraffers/jpql/section06/join/JoinRepository.java
+++ 08_JPA/01_lecture-source/chap04-jpql/src/main/java/com/ohgiraffers/jpql/section06/join/JoinRepository.java
@@ -17,5 +17,3 @@ public interface JoinRepository extends JpaRepository<Menu, Integer> {
     @Query("SELECT DISTINCT c FROM Section06Category c LEFT JOIN c.menuList m")
     List<Category> findAllMenusInCategory();
 }
-
-
