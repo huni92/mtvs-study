417b6f1d69582d7d14038ef3021b2e56b0736141
huni92 <gns2862@naver.com>
2025-01-13 16:21:42 +0900

    jpql

--- 08_JPA/01_lecture-source/chap04-jpql/src/main/java/com/ohgiraffers/jpql/section06/join/JoinRepository.java
+++ 08_JPA/01_lecture-source/chap04-jpql/src/main/java/com/ohgiraffers/jpql/section06/join/JoinRepository.java
@@ -17,3 +17,4 @@ public interface JoinRepository extends JpaRepository<Menu, Integer> {
     @Query("SELECT DISTINCT c FROM Section06Category c LEFT JOIN c.menuList m")
     List<Category> findAllMenusInCategory();
 }
+
